612f4d178dbbc99cfe94b33f5f91e1336a1325b8
mertayda <48617781+mertayda@users.noreply.github.com>
2024-11-21 17:58:14 +0300

    cxcc

---
+++ script.js
@@ -0,0 +1,10 @@
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+    
+};
+
+ScrollReveal().reveal(".section__header", scrollRevealOption);
+ScrollReveal().reveal(".header__form", scrollRevealOption);
+ScrollReveal().reveal(".section__container", scrollRevealOption);
