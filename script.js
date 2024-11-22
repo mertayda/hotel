17e64a0a9f969510306304f477d3e7bddde86fd6
mertayda <48617781+mertayda@users.noreply.github.com>
2024-11-22 17:53:03 +0300

    updated

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ const scrollRevealOption = {
 ScrollReveal().reveal(".section__header", scrollRevealOption);
 ScrollReveal().reveal(".header__form", scrollRevealOption);
 ScrollReveal().reveal(".section__container", scrollRevealOption);
+scrollReveal().reveal(".trending__card", {
+    ...scrollRevealOption,
+    interval:500,
+});
+
+
+scrollReveal().reveal(".destination__card", {
+    duration: 1000,
+    interval:500,
+});
